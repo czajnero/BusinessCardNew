5b4f34811d77b72aa80d7d6820e79c5c5a271f33
Czajnero <czajnero@gmail.com>
2019-11-04 00:17:44 +0100

    Auto stash before merge of "gh-pages" and "origin/gh-pages"

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ const menuSlide = () => {
             if (link.style.animation) {
                 link.style.animation = '';
             } else {
-                link.style.animation = `menuLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+                link.style.animation = `menuLinkFade 0.5s ease forwards ${index / 7 + 0.6}s`;
             }
         });
         burger.classList.toggle('toggle');
